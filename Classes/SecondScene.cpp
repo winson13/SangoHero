4c2159f6b83d3c5cbb1704719d1b3cbaa2f1b15f
ZhangLin <zhanglin@192.168.0.100>
2014-08-13 02:11:18 +0800

    add second scene

---
+++ Classes/SecondScene.cpp
@@ -0,0 +1,9 @@
+//
+//  SecondScene.cpp
+//  MyGame
+//
+//  Created by ZhangLin on 14-8-13.
+//
+//
+
+#include "SecondScene.h"
---
+++ Classes/SecondScene.h
@@ -0,0 +1,14 @@
+//
+//  SecondScene.h
+//  MyGame
+//
+//  Created by ZhangLin on 14-8-13.
+//
+//
+
+#ifndef __MyGame__SecondScene__
+#define __MyGame__SecondScene__
+
+#include <iostream>
+
+#endif /* defined(__MyGame__SecondScene__) */
